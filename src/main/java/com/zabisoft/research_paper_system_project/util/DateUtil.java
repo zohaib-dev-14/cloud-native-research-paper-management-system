9b130d33093061f02dd740e6bffa5c15bd87a117
zohaib <zohaibsaraj10@gmail.com>
2026-05-10 21:26:30 +0500

    Access Token Expiry Date Set

--- src/main/java/com/zabisoft/research_paper_system_project/util/DateUtil.java
+++ src/main/java/com/zabisoft/research_paper_system_project/util/DateUtil.java
@@ -5,7 +5,7 @@ import java.util.Date;
 
 public class DateUtil {
     public static Date expirationDateTime() {
-return new Date(System.currentTimeMillis() + 1000 * 60 * 60);
+return new Date(System.currentTimeMillis() + 1000 * 60 * 15);
     }
 
     public static Date currentDate() {
