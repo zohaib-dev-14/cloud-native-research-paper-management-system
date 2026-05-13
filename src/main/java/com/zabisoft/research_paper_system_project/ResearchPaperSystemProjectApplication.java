8621e1a93520622770e9bb111b6bb831f4cfadff
zohaib <zohaibsaraj10@gmail.com>
2026-05-13 23:01:37 +0500

    changes made in gitignore and other files

--- src/main/java/com/zabisoft/research_paper_system_project/ResearchPaperSystemProjectApplication.java
+++ src/main/java/com/zabisoft/research_paper_system_project/ResearchPaperSystemProjectApplication.java
@@ -1,5 +1,6 @@
 package com.zabisoft.research_paper_system_project;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ResearchPaperSystemProjectApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+
+        System.setProperty(
+                "spring.sendgrid.api-key",
+                dotenv.get("SENDGRID_API_KEY")
+        );
+
+        System.setProperty(
+                "spring.sendgrid.proxy.host",
+                dotenv.get("SENDGRID_PROXY_HOST")
+        );
+        System.setProperty(
+                "JWT_SECRET",
+                dotenv.get("SECRET_KEY")
+        );
+
+        System.setProperty(
+                "server.ssl.key-store-password",
+                dotenv.get("SSL_KEY_STORE_PASSWORD")
+        );
         SpringApplication.run(ResearchPaperSystemProjectApplication.class, args);
     }
 
---
+++ src/main/java/com/zabisoft/research_paper_system_project/exception/GlobalExceptionHandling.java
@@ -0,0 +1,29 @@
+package com.zabisoft.research_paper_system_project.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandling {
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> handleRuntime (RuntimeException runtimeException){
+        return ResponseEntity.badRequest().body(runtimeException.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationException(MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
