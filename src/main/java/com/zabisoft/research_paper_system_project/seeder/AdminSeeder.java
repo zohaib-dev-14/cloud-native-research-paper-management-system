e0ce4afabcca46c1b7133f80831ac5dfa7ae3e63
zohaib <zohaibsaraj10@gmail.com>
2026-05-26 22:46:27 +0500

    Admin for the system added

--- src/main/java/com/zabisoft/research_paper_system_project/seeder/AdminSeeder.java
+++ src/main/java/com/zabisoft/research_paper_system_project/seeder/AdminSeeder.java
@@ -3,37 +3,39 @@ package com.zabisoft.research_paper_system_project.seeder;
 import com.zabisoft.research_paper_system_project.entities.User;
 import com.zabisoft.research_paper_system_project.enums.Role;
 import com.zabisoft.research_paper_system_project.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.NonNull;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
-
+@Component
+@RequiredArgsConstructor
 public class AdminSeeder
         implements CommandLineRunner {
 
     private final UserRepository userRepository;
-
     private final PasswordEncoder passwordEncoder;
+    @Value("${ADMIN_NAME}")
+    private String adminName;
 
-    public AdminSeeder(
-            UserRepository userRepository,
-            PasswordEncoder passwordEncoder
-    ) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
+    @Value("${ADMIN_EMAIL}")
+    private String adminEmail;
+
+    @Value("${ADMIN_PASSWORD}")
+    private String adminPassword;
     @Override
     public void run(String @NonNull ... args) {
         boolean adminExists = userRepository
-                        .findByEmail("muhammadzohaibm271@gmail.com")
+                        .findByEmail(adminName)
                         .isPresent();
 
         if (!adminExists) {
             User admin = new User();
             admin.setName("System Admin");
-            admin.setEmail("muhammadzohaibm271@gmail.com");
-            admin.setPassword(passwordEncoder.encode("AdminZohaib2026"));
+            admin.setEmail(adminEmail);
+            admin.setPassword(passwordEncoder.encode(adminPassword));
             admin.setRole(
                     Role.ADMIN
             );
