22fb335206f75d4d0b21f2e4d892ef718e71d122
zohaib <zohaibsaraj10@gmail.com>
2026-05-31 20:08:56 +0500

    cors configuration

---
+++ src/main/java/com/zabisoft/research_paper_system_project/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.zabisoft.research_paper_system_project.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://researchpaper.site"
+                        )
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
