5be7e5f1efa6d75bcbec01bcb68684ddde71b1f6
zohaib <zohaibsaraj10@gmail.com>
2026-06-21 14:25:31 +0500

    fix: otptype readonly

--- src/main/java/com/zabisoft/research_paper_system_project/dto/SendForgetPasswordOTP.java
+++ src/main/java/com/zabisoft/research_paper_system_project/dto/SendForgetPasswordOTP.java
@@ -1,12 +1,12 @@
 package com.zabisoft.research_paper_system_project.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.zabisoft.research_paper_system_project.enums.OTPType;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+
+
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
+
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -23,7 +23,6 @@ public class SendForgetPasswordOTP {
     @Size(max = 30, message = "Email too long")
 
     private String email;
-    @Enumerated(EnumType.STRING)
-    @JsonIgnore
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private OTPType otpType = OTPType.FORGOT_PASSWORD;
 }
