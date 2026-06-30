5d758fdb9c763026760f60fc1ee679dfd6968539
zohaib <zohaibsaraj10@gmail.com>
2026-06-30 12:32:29 +0500

    fix: old library used in code

--- src/main/java/com/zabisoft/research_paper_system_project/config/MinioConfig.java
+++ src/main/java/com/zabisoft/research_paper_system_project/config/MinioConfig.java
@@ -4,7 +4,6 @@ package com.zabisoft.research_paper_system_project.config;
 
 import io.minio.Http;
 import io.minio.MinioClient;
-import org.apache.http.protocol.HTTP;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
