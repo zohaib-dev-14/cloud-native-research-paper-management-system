45ca5d21e273ce361fb0ceba2e804ffa7c3c2fff
zohaib <zohaibsaraj10@gmail.com>
2026-07-02 11:46:32 +0500

    cors error solve fix

--- src/main/java/com/zabisoft/research_paper_system_project/config/CorsConfig.java
+++ src/main/java/com/zabisoft/research_paper_system_project/config/CorsConfig.java
@@ -9,6 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig {
 
     @Bean
+
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
