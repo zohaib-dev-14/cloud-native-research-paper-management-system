de94bc11afa40fd5bf1101cf70a0bd82625746f7
zohaib <zohaibsaraj10@gmail.com>
2026-07-02 11:23:36 +0500

    cors error solve fix

---
+++ src/main/java/com/zabisoft/research_paper_system_project/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.zabisoft.research_paper_system_project.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://researchpaper.site", "https://www.researchpaper.site") // 🎯 Tumhaare domains yahan hone chahiye
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
